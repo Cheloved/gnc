15e4393868143044adfefada63d6201f80cb0ef2
Cheloved <27046990+Cheloved@users.noreply.github.com>
2023-07-26 20:38:51 +0300

    s_host struct added

--- backend/include/defines.h
+++ backend/include/defines.h
@@ -3,4 +3,20 @@
 
 #define BUFFER_SIZE 8192
 
+// Info about single host
+struct s_host
+{
+    // Connection data
+    char ip[16]; // IP in human-readable format
+    int ip_bin;  // IP in binary representation
+    int port;
+
+    // Amount of threads it wills to share
+    int threads;
+
+    // Additional info (optional)
+    char* extra_info;
+    int extra_len;
+}
+
 #endif
